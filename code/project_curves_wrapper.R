1ad41f35ef6154d7590d445ea7e0a6b3befc746e
megan-k-ayers <m.ayers@yale.edu>
2025-09-25 13:50:51 +0100

    Parallelized project x params loops

--- code/project_curves_wrapper.R
+++ code/project_curves_wrapper.R
@@ -8,11 +8,28 @@ library(dplyr)
 source(here("code", "spec_curve", "create_spec_chart_function.R"))
 
 
+# Function to add project names into param list for easier looping.
+grid_helper <- function(ind_p_list, projects) {
+  ind_p_list$project <- sapply(projects, function(p) p[1])
+  return(ind_p_list)
+}
+
+
+# Keep a record of current progress (overwrites every time).
+update_log <- function(project_name, grid_row, total) {
+  write(paste0(Sys.time(), ": Worker ", Sys.getpid(), " finished grid row ",
+               grid_row, "/", total, " for project ", project_name, "."),
+        "data/progress.log", append = TRUE)
+}
+
+
+### Main wrapper function.
 make_sc_curves <- function(projects, ate_method, p_list, plot_only = FALSE,
                            leftmargin = 5) {
   
-  
-  if (!plot_only) {
+  ### This block will perform all preprocessing and method execution. 
+  if (!plot_only) { 
+    
     # Load processing and logic functions for the given method.
     # NOTE: This means `ate_method` must match the name of the method directory,
     # and each directory must have files with these specific names, containing
@@ -27,55 +44,64 @@ make_sc_curves <- function(projects, ate_method, p_list, plot_only = FALSE,
                    "."))
     }
     
-    # Create grid of parameter permutations.
+    # Create grid of parameter permutations x projects, to allow parallelization
+    # over both parameters and projects.
     if (ate_method == "synthetic_controls") {
       # Different flavors of synthetic controls need different parameters, but
       # want to see them in the same plot.
-      p_grid_g <- expand.grid(p_list$gysnth)
-      p_grid_m <- expand.grid(p_list$microsynth)
-      p_grid_a <- expand.grid(p_list$augsynth)
+      p_grid_g <- expand.grid(grid_helper(p_list$gysnth, projects),
+                              stringsAsFactors = FALSE)
+      p_grid_m <- expand.grid(grid_helper(p_list$microsynth, projects),
+                              stringsAsFactors = FALSE)
+      p_grid_a <- expand.grid(grid_helper(p_list$augsynth, projects),
+                              stringsAsFactors = FALSE)
       p_grid <- plyr::rbind.fill(list(p_grid_g,  # Pads non-shared columns
                                       p_grid_m, p_grid_a))
     } else {
-      p_grid <- expand.grid(p_list, stringsAsFactors = FALSE)
+      p_grid <- expand.grid(grid_helper(p_list, projects),
+                            stringsAsFactors = FALSE)
     }
     
     
     # Iterate through all possible permutations for each project.
-    for (project in projects) {
-      curr_proj_results <- data.frame(project_name = character(),
-                                      year = numeric(), ATT = numeric(),
-                                      lower = numeric(), upper = numeric())
-      for (i in 1:nrow(p_grid)) {
-        params <- p_grid[i, ]
-        
-        # Call method execution function for the given parameter combination.
-        print(paste0("Starting ", ate_method, " for ", project[1],
-                     " with parameter setting ", i, "/", nrow(p_grid), "."))
-        ates_by_year <- execute_method(project_name = project[1],
-                                       start_year = as.numeric(project[2]), 
-                                       params = params)
-        
-        # Retrieve the ATT, lower and upper CI bounds for the year 2022.
-        # NOTE: May want to make year an input variable, or a project-specific
-        # feature saved in the `projects` list with name and start year.
-        result_2022 <- ates_by_year %>% filter(year == 22)
-        curr_proj_results <- rbind(curr_proj_results, data.frame(
-          project_name = project[1],
-          year = project[2],
-          ATT = result_2022$coef,
-          lower = result_2022$lower,
-          upper = result_2022$upper
-        ))
-      }
+    write("", file = "data/progress.log")
+    curr_results <- foreach(i = 1:nrow(p_grid), .combine = rbind) %dopar% {
+      params <- p_grid[i, names(p_grid) != "project"]
+      proj_names <- sapply(projects, function(p) p[1])
+      project <- projects[proj_names == p_grid[i, "project"]][[1]]
       
-      # Record all ATT estimates for this project + parameter settings used.
-      curr_proj_results <- cbind(curr_proj_results, p_grid)
-      saveRDS(curr_proj_results, paste0("data/results/", ate_method, "/",
-                                        project[1], ".rds"))
+      # Call method execution function for the given parameter combination.
+      # print(paste0("Starting ", ate_method, " for ", project[1],
+      #              " with parameter setting ", i, "/", nrow(p_grid), "."))
+      ates_by_year <- execute_method(project_name = project[1],
+                                     start_year = as.numeric(project[2]), 
+                                     params = params)
+      
+      update_log(project[1], i, nrow(p_grid))  # Record progress
+      
+      # Retrieve the ATT, lower and upper CI bounds for the year 2022.
+      # NOTE: May want to make year an input variable, or a project-specific
+      # feature saved in the `projects` list with name and start year.
+      result_2022 <- ates_by_year %>% filter(year == 22)
+      
+      return(data.frame(project_name = project[1], year = project[2],
+                        ATT = result_2022$coef, lower = result_2022$lower,
+                        upper = result_2022$upper))
+    }
+    
+    # Add parameter information back 
+    curr_results <- cbind(curr_results, p_grid[, names(p_grid) != "project"])
+  
+    # Record all ATT estimates separately for each project.
+    for (project in projects) {
+      these <- curr_results[curr_results$project_name == project[1], ]
+      saveRDS(these, paste0("data/results/", ate_method, "/", project[1],
+                            ".rds"))
     }
   }
-    
+  
+
+  ### Make specification curves for each project.
   for (project in projects) {
     curr_proj_results <- readRDS(paste0("data/results/", ate_method, "/",
                                         project[1], ".rds"))
--- code/run_project_curves.R
+++ code/run_project_curves.R
@@ -5,42 +5,44 @@
 
 rm(list = ls())
 library(here)
+library(doParallel)
+registerDoParallel(cores = 5)
 source(here("code", "project_curves_wrapper.R"))
 source(here('code', 'Projects', 'universal_list_of_projects.R'))
 
-projects <- get_projects()[1]
+projects <- get_projects()[1:2]
 
 ### Uncomment / adjust p_list to run for MATCHING.
-# ate_method <- "matching"
-# p_list <- list(method = c("nearest", "cem"),
-#                distance = c("logit", "mahalanobis", "euclidean"),
-#                ratio = c(1, 3, 5),
-#                covariates = list(c("treecover_past", "accessibility",
-#                                    "accessibility_walking_only", "aspect",
-#                                    "elevation", "slope"),
-#                                  c("treecover_past", "accessibility")))
+ate_method <- "matching"
+p_list <- list(method = c("nearest", "cem"),
+               distance = c("logit", "mahalanobis", "euclidean"),
+               ratio = c(1, 3, 5),
+               covariates = list(c("treecover_past", "accessibility",
+                                   "accessibility_walking_only", "aspect",
+                                   "elevation", "slope"),
+                                 c("treecover_past", "accessibility")))
 
 
 ### Uncomment / adjust p_list to run for SYNTHETIC CONTROLS.
-ate_method <- "synthetic_controls"
-p_list_g <- list(sc_method = "gsynth",
-                 force = c("none", "unit", "time", "two-way"),
-                 estimator = c("ife"),
-                 r = c(1, 3, 5, "cv"))
-p_list_m <- list(sc_method = "microsynth",
-                 covariates = list(c("treecover_2000", "hillshade", "aspect",
-                                     "elevation", "slope"),
-                                   c("elevation", "slope")))
-p_list_a <- list(sc_method = "augsynth",
-                 inf_type = c("conformal", "jackknife"),
-                 covariates = list(c("treecover_2000", "hillshade", "aspect",
-                                     "elevation", "slope"),
-                                   c("elevation", "slope")),
-                 progfunc = c("Ridge", "None", "RF"),
-                 force = c("none", "two-way")) 
-# p_list <- list("gysnth" = p_list_g, "microsynth" = p_list_m,
-#                "augsynth" = p_list_a)
-p_list <- list("augsynth" = p_list_a)
+# ate_method <- "synthetic_controls"
+# p_list_g <- list(sc_method = "gsynth",
+#                  force = c("none", "unit", "time", "two-way"),
+#                  estimator = c("ife"),
+#                  r = c(1, 3, 5, "cv"))
+# p_list_m <- list(sc_method = "microsynth",
+#                  covariates = list(c("treecover_2000", "hillshade", "aspect",
+#                                      "elevation", "slope"),
+#                                    c("elevation", "slope")))
+# p_list_a <- list(sc_method = "augsynth",
+#                  inf_type = c("conformal", "jackknife"),
+#                  covariates = list(c("treecover_2000", "hillshade", "aspect",
+#                                      "elevation", "slope"),
+#                                    c("elevation", "slope")),
+#                  progfunc = c("Ridge", "None", "RF"),
+#                  force = c("none", "two-way")) 
+# # p_list <- list("gysnth" = p_list_g, "microsynth" = p_list_m,
+# #                "augsynth" = p_list_a)
+# p_list <- list("augsynth" = p_list_a)
 
 
 make_sc_curves(projects, ate_method, p_list, plot_only = FALSE,
