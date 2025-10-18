850e2ec8e20716dd0afe889fc838a363f2c019b5
Megan Ayers <ma984@a1132u09n04.mghpcc.ycrc.yale.edu>
2025-10-18 09:42:43 -0400

    Cluster package setup

--- code/setup.R
+++ code/setup.R
@@ -1,31 +1,32 @@
 # Author: Megan Ayers
 # Date: 10/11/2025
-# Function: Load all packages and run function scripts.
+# Function: Load (or install) all packages and run function scripts.
 
-# devtools::install_github("ebenmichael/augsynth")
-# install_github("susanathey/MCPanel")
-library(augsynth)
-library(here)
-library(doParallel)
-library(estimatr)
-library(ggplot2)
-library(glmnet)
-library(grf)
-library(gsynth)
-library(MatchIt)
-library(MCPanel)
-library(microsynth)
-library(randomForest)
-library(sf)
-library(stringr)
-library(tidyr)
-library(tidyverse)
-library(tidyquant)
-library(zoo)
-library(dplyr)
+# Install augsynth, which isn't on CRAN:
+if (!"augsynth" %in% installed.packages()) {
+  devtools::install_github("ebenmichael/augsynth") 
+}
 
+# For installing gsynth properly given this version of R:
+if (!"gsynth" %in% installed.packages()) {
+  remotes::install_version("ggplot2", version = "3.4.4")
+  remotes::install_version("GGally", version = "2.1.2")
+  install.packages("gsynth")
+}
 
+packages <- c("augsynth", "here", "doParallel", "estimatr", "ggplot2", "glmnet",
+              "grf", "gsynth", "MatchIt", "MCPanel", "microsynth",
+              "randomForest", "sf", "stringr", "tidyr", "tidyverse", 
+              "tidyquant", "zoo", "dplyr")
+
+# Install any missing packages.
+to_install <- packages[!packages %in% installed.packages()[, "Package"]]
+if (length(to_install) > 0) install.packages(to_install)
+
+# Load all
+invisible(lapply(packages, library, character.only = TRUE))
+
+# Load helper functions
 source(here("code", "project_curves_wrapper.R"))
 source(here('code', 'Projects', 'universal_list_of_projects.R'))
-
 
